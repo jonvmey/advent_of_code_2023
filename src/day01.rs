895d151c069cfab24dc8dde4dc052dfdd328f7f8
Jonathan VanderMey <jonathan@vandermey.ca>
2023-12-01 14:25:22 -0500

    Set up basic project structure

---
+++ src/day01.rs
@@ -0,0 +1,11 @@
+use aoc_runner_derive::{aoc, aoc_generator};
+
+#[aoc_generator(day1)]
+fn parse_input(_input: &str) -> Vec<String> {
+    vec![]
+}
+
+#[aoc(day1, part1)]
+fn part1(_vals: &[String]) -> usize {
+    0
+}
