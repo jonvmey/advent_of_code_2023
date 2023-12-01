2b18c8defbeaf82fd14b7e8c3c1fa96d637eaf6c
Jonathan VanderMey <jonathan@vandermey.ca>
2023-12-01 14:53:33 -0500

    Add day 1 part 1 solution

--- src/day01.rs
+++ src/day01.rs
@@ -1,11 +1,18 @@
 use aoc_runner_derive::{aoc, aoc_generator};
 
+fn process_line_part_1(line : &str) -> i32 {
+    let first_digit = line.chars().find(|&c| c.is_digit(10)).unwrap().to_string();
+    let last_digit = line.chars().rev().find(|&c| c.is_digit(10)).unwrap().to_string();
+
+    first_digit.parse::<i32>().unwrap() * 10 + last_digit.parse::<i32>().unwrap()
+}
+
 #[aoc_generator(day1)]
 fn parse_input(input: &str) -> Vec<String> {
     input.lines().map(|line| line.to_string()).collect()
 }
 
 #[aoc(day1, part1)]
-fn part1(vals: &[String]) -> usize {
-    0
+fn part1(lines: &[String]) -> i32 {
+    lines.iter().map(|line| process_line_part_1(line)).sum()
 }
