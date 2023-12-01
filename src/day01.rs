c2ab0d2474ad2fa28e443a83bbc178e09b9cf32f
Jonathan VanderMey <jonathan@vandermey.ca>
2023-12-01 14:30:32 -0500

    Parse input

--- src/day01.rs
+++ src/day01.rs
@@ -1,11 +1,11 @@
 use aoc_runner_derive::{aoc, aoc_generator};
 
 #[aoc_generator(day1)]
-fn parse_input(_input: &str) -> Vec<String> {
-    vec![]
+fn parse_input(input: &str) -> Vec<String> {
+    input.lines().map(|line| line.to_string()).collect()
 }
 
 #[aoc(day1, part1)]
-fn part1(_vals: &[String]) -> usize {
+fn part1(vals: &[String]) -> usize {
     0
 }
