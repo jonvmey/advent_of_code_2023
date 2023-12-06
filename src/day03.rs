64a7157a4f1296a695d6a7680b45bb274fa4d261
Jonathan VanderMey <jonathan@vandermey.ca>
2023-12-05 23:35:20 -0500

    Add day 3 structure

---
+++ src/day03.rs
@@ -0,0 +1,16 @@
+use aoc_runner_derive::{aoc, aoc_generator};
+
+#[aoc_generator(day3)]
+fn parse_input(input: &str) -> String {
+    input.to_string() 
+}
+
+#[aoc(day3, part1)]
+fn part1(_input: &str) -> u32 {
+    0
+}
+
+#[aoc(day3, part2)]
+fn part2(_input: &str) -> u32 {
+    0
+}
