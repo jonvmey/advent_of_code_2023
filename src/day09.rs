8e677d65781aa398232ddf7bb32e1afc7bc2380d
Jonathan VanderMey <jonathan@vandermey.ca>
2023-12-09 00:31:36 -0500

    Add day 9 structure

---
+++ src/day09.rs
@@ -0,0 +1,15 @@
+use aoc_runner_derive::{aoc, aoc_generator};
+
+#[aoc_generator(day9)]
+fn parse_input(_input: &str) -> () {
+}
+
+#[aoc(day9, part1)]
+fn part1(_input: &()) -> u32 {
+    0
+}
+
+#[aoc(day9, part2)]
+fn part2(_input: &()) -> u64 {
+    0
+}
