ce61daa4665d7e23176db7f07854cb3d11f2d318
Jonathan VanderMey <jonathan@vandermey.ca>
2023-12-10 02:29:24 -0500

    Add day 10 part 1 solution

--- src/day10.rs
+++ src/day10.rs
@@ -1,15 +1,116 @@
 use aoc_runner_derive::{aoc, aoc_generator};
+use grid_2d::{Coord, Grid, Size};
+
+#[derive(Copy, Clone, Hash, Eq, PartialEq)]
+enum Direction {
+    North,
+    East,
+    South,
+    West,
+}
+
+#[derive(Copy, Clone, PartialEq)]
+enum Tile {
+    NorthSouth, // |
+    EastWest,   // -
+    NorthEast,  // L
+    NorthWest,  // J
+    SouthWest,  // 7
+    SouthEast,  // F
+    Ground,     // .
+    Start,      // S
+}
+
+fn char_to_tile(c: char) -> Tile {
+    match c {
+        '|' => Tile::NorthSouth,
+        '-' => Tile::EastWest,
+        'L' => Tile::NorthEast,
+        'J' => Tile::NorthWest,
+        '7' => Tile::SouthWest,
+        'F' => Tile::SouthEast,
+        '.' => Tile::Ground,
+        'S' => Tile::Start,
+        _ => panic!(),
+    }
+}
+
+fn adjacent_tile(location: Coord, direction: Direction) -> Coord {
+    match direction {
+        Direction::North => Coord::new(location.x, location.y - 1),
+        Direction::East => Coord::new(location.x + 1, location.y),
+        Direction::South => Coord::new(location.x, location.y + 1),
+        Direction::West => Coord::new(location.x - 1, location.y),
+    }
+}
+
+fn connected_directions(tile: Tile) -> [Direction; 2] {
+    match tile {
+        Tile::NorthSouth => [Direction::North, Direction::South],
+        Tile::EastWest => [Direction::East, Direction::West],
+        Tile::NorthEast => [Direction::North, Direction::East],
+        Tile::NorthWest => [Direction::North, Direction::West],
+        Tile::SouthWest => [Direction::South, Direction::West],
+        Tile::SouthEast => [Direction::South, Direction::East],
+        Tile::Start | Tile::Ground => panic!(),
+    }
+}
+
+fn connected_tiles(grid: &Grid<Tile>, tile: Coord) -> Vec<Coord> {
+    connected_directions(*grid.get(tile).unwrap())
+        .iter()
+        .map(|direction| adjacent_tile(tile, *direction))
+        .collect()
+}
 
 #[aoc_generator(day10)]
-fn parse_input(_input: &str) -> () {
+fn parse_input(input: &str) -> (Grid<Tile>, Coord) {
+    const WIDTH: u32 = 140;
+    const HEIGHT: u32 = 140;
+
+    let mut grid = Grid::new_iterator(
+        Size::new(WIDTH, HEIGHT),
+        input.chars().filter(|c| *c != '\n').map(char_to_tile),
+    );
+
+    let (start, _) = grid
+        .enumerate()
+        .find(|(_, value)| *value == &Tile::Start)
+        .unwrap();
+    *grid.get_mut(start).unwrap() = Tile::SouthWest;
+
+    (grid, start)
 }
 
 #[aoc(day10, part1)]
-fn part1(_input: &()) -> i32 {
-    0
+fn part1((grid, start): &(Grid<Tile>, Coord)) -> i32 {
+    let mut loop_length = 0;
+
+    let mut current_position = *start;
+    let mut previous_position = adjacent_tile(*start, Direction::South);
+
+    loop {
+        let connected = connected_tiles(grid, current_position);
+
+        let next_position = if connected[0] == previous_position {
+            connected[1]
+        } else {
+            connected[0]
+        };
+        previous_position = current_position;
+        current_position = next_position;
+
+        loop_length += 1;
+
+        if current_position == *start {
+            break;
+        }
+    }
+
+    loop_length / 2
 }
 
 #[aoc(day10, part2)]
-fn part2(_input: &()) -> i32 {
+fn part2(_input: &(Grid<Tile>, Coord)) -> i32 {
     0
 }
