bafb0586251b5fd456395d4573ba150d5b404723
Jonathan VanderMey <jonathan@vandermey.ca>
2023-12-10 00:35:20 -0500

    Add day 10 structure

---
+++ src/day10.rs
@@ -0,0 +1,15 @@
+use aoc_runner_derive::{aoc, aoc_generator};
+
+#[aoc_generator(day10)]
+fn parse_input(_input: &str) -> () {
+}
+
+#[aoc(day10, part1)]
+fn part1(_input: &()) -> i32 {
+    0
+}
+
+#[aoc(day10, part2)]
+fn part2(_input: &()) -> i32 {
+    0
+}
