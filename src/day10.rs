4b7d6359a3448a59bd6cc8759541b80c25adf6c5
Jonathan VanderMey <jonathan@vandermey.ca>
2023-12-11 11:06:41 -0500

    Add day 10 part 2 implementation
    Also cleaned up day 10 code a little.

--- src/day10.rs
+++ src/day10.rs
@@ -9,7 +9,7 @@ enum Direction {
     West,
 }
 
-#[derive(Copy, Clone, PartialEq)]
+#[derive(Copy, Clone, Debug, PartialEq)]
 enum Tile {
     NorthSouth, // |
     EastWest,   // -
@@ -56,40 +56,21 @@ fn connected_directions(tile: Tile) -> [Direction; 2] {
     }
 }
 
-fn connected_tiles(grid: &Grid<Tile>, tile: Coord) -> Vec<Coord> {
-    connected_directions(*grid.get(tile).unwrap())
+fn connected_tiles(grid: &Grid<(Tile, bool)>, tile: Coord) -> Vec<Coord> {
+    connected_directions(grid.get(tile).unwrap().0)
         .iter()
         .map(|direction| adjacent_tile(tile, *direction))
         .collect()
 }
 
-#[aoc_generator(day10)]
-fn parse_input(input: &str) -> (Grid<Tile>, Coord) {
-    const WIDTH: u32 = 140;
-    const HEIGHT: u32 = 140;
-
-    let mut grid = Grid::new_iterator(
-        Size::new(WIDTH, HEIGHT),
-        input.chars().filter(|c| *c != '\n').map(char_to_tile),
-    );
-
-    let (start, _) = grid
-        .enumerate()
-        .find(|(_, value)| *value == &Tile::Start)
-        .unwrap();
-    *grid.get_mut(start).unwrap() = Tile::SouthWest;
-
-    (grid, start)
-}
-
-#[aoc(day10, part1)]
-fn part1((grid, start): &(Grid<Tile>, Coord)) -> i32 {
+fn find_loop(grid: &mut Grid<(Tile, bool)>, start: Coord) -> u32 {
     let mut loop_length = 0;
-
-    let mut current_position = *start;
-    let mut previous_position = adjacent_tile(*start, Direction::South);
+    let mut current_position = start;
+    let mut previous_position = adjacent_tile(start, Direction::South);
 
     loop {
+        grid.get_mut(current_position).unwrap().1 = true;
+
         let connected = connected_tiles(grid, current_position);
 
         let next_position = if connected[0] == previous_position {
@@ -102,15 +83,63 @@ fn part1((grid, start): &(Grid<Tile>, Coord)) -> i32 {
 
         loop_length += 1;
 
-        if current_position == *start {
+        if current_position == start {
             break;
         }
     }
 
-    loop_length / 2
+    loop_length
+}
+
+#[aoc_generator(day10)]
+fn parse_input(input: &str) -> (Grid<(Tile, bool)>, Coord) {
+    const WIDTH: u32 = 140;
+    const HEIGHT: u32 = 140;
+
+    let mut grid = Grid::new_iterator(
+        Size::new(WIDTH, HEIGHT),
+        input
+            .chars()
+            .filter(|c| *c != '\n')
+            .map(|c| (char_to_tile(c), false)),
+    );
+
+    let (start, _) = grid
+        .enumerate()
+        .find(|(_, (tile, _))| *tile == Tile::Start)
+        .unwrap();
+    grid.get_mut(start).unwrap().0 = Tile::SouthWest;
+
+    (grid, start)
+}
+
+#[aoc(day10, part1)]
+fn part1((grid, start): &(Grid<(Tile, bool)>, Coord)) -> u32 {
+    let mut grid = Grid::new_iterator(grid.size(), grid.iter().copied());
+
+    find_loop(&mut grid, *start) / 2
 }
 
 #[aoc(day10, part2)]
-fn part2(_input: &(Grid<Tile>, Coord)) -> i32 {
-    0
+fn part2((grid, start): &(Grid<(Tile, bool)>, Coord)) -> u32 {
+    let mut grid = Grid::new_iterator(grid.size(), grid.iter().copied());
+
+    find_loop(&mut grid, *start);
+
+    let mut inside_loop = false;
+    let mut inside_count = 0;
+
+    for (tile, part_of_loop) in grid.iter() {
+        if *part_of_loop {
+            match tile {
+                Tile::NorthSouth | Tile::NorthWest | Tile::NorthEast => inside_loop = !inside_loop,
+                _ => (),
+            }
+        } else if inside_loop {
+            inside_count += 1;
+        }
+
+    }
+
+    inside_count
 }
