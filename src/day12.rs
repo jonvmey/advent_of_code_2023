75acdcb772a6f523bda75afa70a2269afb09d2a0
Jonathan VanderMey <jonathan@vandermey.ca>
2023-12-12 01:07:39 -0500

    Add day 12 structure

---
+++ src/day12.rs
@@ -0,0 +1,15 @@
+use aoc_runner_derive::{aoc, aoc_generator};
+
+#[aoc_generator(day12)]
+fn parse_input(_input: &str) -> () {
+}
+
+#[aoc(day12, part1)]
+fn part1(_input: &()) -> u32 {
+    0
+}
+
+#[aoc(day12, part2)]
+fn part2(_input: &()) -> u32 {
+    0
+}
--- src/lib.rs
+++ src/lib.rs
@@ -9,5 +9,6 @@ mod day08;
 mod day09;
 mod day10;
 mod day11;
+mod day12;
 
 aoc_runner_derive::aoc_lib! {year = 2023 }
