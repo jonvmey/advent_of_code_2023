a5f50e058956efa046ceb866a6ad6994445d200b
Jonathan VanderMey <jonathan@vandermey.ca>
2023-12-14 10:24:43 -0500

    Add day 13 part 1 solution

--- src/day13.rs
+++ src/day13.rs
@@ -1,19 +1,203 @@
 use aoc_runner_derive::{aoc, aoc_generator};
+use grid_2d::{Grid, Size};
+use nom::branch::alt;
+use nom::bytes::complete::tag;
+use nom::character::complete::newline;
+use nom::multi::{many1, separated_list1};
+use nom::IResult;
+use std::collections::HashSet;
+
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+enum Axis {
+    Vertical,
+    Horizontal,
+}
+
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+enum Terrain {
+    Ash,
+    Rock,
+}
+
+impl From<char> for Terrain {
+    fn from(c: char) -> Self {
+        match c {
+            '.' => Terrain::Ash,
+            '#' => Terrain::Rock,
+            _ => panic!(),
+        }
+    }
+}
+
+fn calculate_intersections(mut possible_mirror_points: Vec<HashSet<usize>>) -> Option<usize> {
+    let (intersection, others) = possible_mirror_points.split_at_mut(1);
+    let intersection = &mut intersection[0];
+    for other in others {
+        intersection.retain(|e| other.contains(e));
+    }
+
+    if intersection.len() == 1 {
+        return Some(*intersection.iter().next().unwrap());
+    }
+
+    None
+}
+
+fn check_vertical_mirror(grid: &Grid<Terrain>) -> Option<usize> {
+    let width = grid.width() as usize;
+    let mut possible_mirror_point_list: Vec<HashSet<usize>> = vec![];
+
+    for row in grid.rows() {
+        let mut possible_mirror_points = HashSet::new();
+
+        for i in 0..width - 1 {
+            if row[..i + 1]
+                .iter()
+                .rev()
+                .zip(row[i + 1..].iter())
+                .all(|(a, b)| a == b)
+            {
+                possible_mirror_points.insert(i + 1);
+            }
+        }
+        possible_mirror_point_list.push(possible_mirror_points.clone());
+
+        if possible_mirror_points.is_empty() {
+            return None;
+        }
+    }
+
+    calculate_intersections(possible_mirror_point_list)
+}
+
+fn check_horizontal_mirror(grid: &Grid<Terrain>) -> Option<usize> {
+    let height = grid.height() as usize;
+    let width = grid.width() as usize;
+    let mut possible_mirror_point_list: Vec<HashSet<usize>> = vec![];
+
+    for column_index in 0..width {
+        let column: Vec<Terrain> = grid
+            .iter()
+            .skip(column_index)
+            .step_by(width)
+            .copied()
+            .collect();
+        let mut possible_mirror_points = HashSet::new();
+
+        for i in 0..height - 1 {
+            if column[..i + 1]
+                .iter()
+                .rev()
+                .zip(column[i + 1..].iter())
+                .all(|(a, b)| a == b)
+            {
+                possible_mirror_points.insert(i + 1);
+            }
+        }
+
+        if possible_mirror_points.is_empty() {
+            return None;
+        }
+
+        possible_mirror_point_list.push(possible_mirror_points);
+    }
+
+    calculate_intersections(possible_mirror_point_list)
+}
+
+fn find_mirror_location(grid: &Grid<Terrain>) -> (usize, Axis) {
+    if let Some(mirror_line) = check_vertical_mirror(grid) {
+        return (mirror_line, Axis::Vertical);
+    }
+    if let Some(mirror_line) = check_horizontal_mirror(grid) {
+        return (mirror_line, Axis::Horizontal);
+    }
+
+    panic!("could not find a mirroring");
+}
+
+fn parse_grid_line(input: &str) -> IResult<&str, Vec<Terrain>> {
+    let (input, cells) = many1(alt((tag("."), tag("#"))))(input)?;
+
+    Ok((
+        input,
+        cells
+            .into_iter()
+            .map(|c| Terrain::from(c.chars().next().unwrap()))
+            .collect(),
+    ))
+}
+
+fn parse_grid(input: &str) -> IResult<&str, Grid<Terrain>> {
+    let (input, lines) = separated_list1(newline, parse_grid_line)(input)?;
+
+    Ok((
+        input,
+        Grid::new_iterator(
+            Size::new(lines.first().unwrap().len() as u32, lines.len() as u32),
+            lines.into_iter().flatten(),
+        ),
+    ))
+}
 
 #[aoc_generator(day13)]
-fn parse_input(_input: &str) -> () {
+fn parse_input(input: &str) -> Vec<Grid<Terrain>> {
+    let (_, grids) = separated_list1(tag("\n\n"), parse_grid)(input).unwrap();
+
+    grids
 }
 
 #[aoc(day13, part1)]
-fn part1(_input: &()) -> u64 {
-    0
+fn part1(grids: &[Grid<Terrain>]) -> usize {
+    grids
+        .iter()
+        .map(|grid| match find_mirror_location(grid) {
+            (line, Axis::Vertical) => line,
+            (line, Axis::Horizontal) => 100 * line,
+        })
+        .sum()
 }
 
 #[aoc(day13, part2)]
-fn part2(_input: &()) -> u64 {
+fn part2(_grids: &[Grid<Terrain>]) -> usize {
     0
 }
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
+    #[test]
+    fn test1() {
+        let input = concat!(
+            "#.##..##.\n",
+            "..#.##.#.\n",
+            "##......#\n",
+            "##......#\n",
+            "..#.##.#.\n",
+            "..##..##.\n",
+            "#.#.##.#.\n",
+        );
+        let grid = parse_input(input);
+        let (mirror_line, axis) = find_mirror_location(&grid[0]);
+        assert_eq!(mirror_line, 5);
+        assert_eq!(axis, Axis::Vertical);
+    }
+
+    #[test]
+    fn test2() {
+        let input = concat!(
+            "#...##..#\n",
+            "#....#..#\n",
+            "..##..###\n",
+            "#####.##.\n",
+            "#####.##.\n",
+            "..##..###\n",
+            "#....#..#\n",
+        );
+        let grid = parse_input(input);
+        let (mirror_line, axis) = find_mirror_location(&grid[0]);
+        assert_eq!(mirror_line, 4);
+        assert_eq!(axis, Axis::Horizontal);
+    }
 }
