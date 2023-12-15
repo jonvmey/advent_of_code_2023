0a01676f1a6bce9574bfe5d95d1c229f3ffd9633
Jonathan VanderMey <jonathan@vandermey.ca>
2023-12-15 09:29:03 -0500

    Add day 15 structure

---
+++ src/day15.rs
@@ -0,0 +1,19 @@
+use aoc_runner_derive::{aoc, aoc_generator};
+
+#[aoc_generator(day15)]
+fn parse_input(_input: &str) -> () {
+}
+
+#[aoc(day15, part1)]
+fn part1(_input: &()) -> usize {
+    0
+}
+
+#[aoc(day15, part2)]
+fn part2(_input: &()) -> usize {
+    0
+}
+
+#[cfg(test)]
+mod tests {
+}
