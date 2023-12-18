b9d6628325f06fb671db0d68f65ab5dde3d0f6c1
Jonathan VanderMey <jonathan@vandermey.ca>
2023-12-18 00:28:35 -0500

    Formatting

--- src/day17.rs
+++ src/day17.rs
@@ -21,12 +21,16 @@ struct HeapState {
 
 impl HeapState {
     fn new(location: Coord, came_from: Coord, straight_line_count: u32, cost: u32) -> Self {
-        HeapState{location, came_from, straight_line_count, cost}
+        HeapState {
+            location,
+            came_from,
+            straight_line_count,
+            cost,
+        }
     }
 }
 
-impl Eq for HeapState {
-}
+impl Eq for HeapState {}
 
 impl PartialEq for HeapState {
     fn eq(&self, other: &Self) -> bool {
@@ -47,7 +51,12 @@ impl PartialOrd for HeapState {
     }
 }
 
-fn get_potential_neighbours(location: Coord, came_from: Coord, can_continue_straight: bool, can_turn: bool) -> Vec<(Coord, Direction)> {
+fn get_potential_neighbours(
+    location: Coord,
+    came_from: Coord,
+    can_continue_straight: bool,
+    can_turn: bool,
+) -> Vec<(Coord, Direction)> {
     let mut neighbours = vec![];
 
     let delta_x = location.x - came_from.x;
@@ -55,31 +64,35 @@ fn get_potential_neighbours(location: Coord, came_from: Coord, can_continue_stra
 
     if delta_y == 0 {
         if can_turn {
-            neighbours.push((Coord::new(location.x, location.y+1), Direction::Turned));
-            neighbours.push((Coord::new(location.x, location.y-1), Direction::Turned));
+            neighbours.push((Coord::new(location.x, location.y + 1), Direction::Turned));
+            neighbours.push((Coord::new(location.x, location.y - 1), Direction::Turned));
         }
 
         if can_continue_straight {
-            if delta_x == 1 { // came from west, continue east
-                neighbours.push((Coord::new(location.x+1, location.y), Direction::Straight));
+            if delta_x == 1 {
+                // came from west, continue east
+                neighbours.push((Coord::new(location.x + 1, location.y), Direction::Straight));
             }
-            if delta_x == -1 { // came from east, continue west
-                neighbours.push((Coord::new(location.x-1, location.y), Direction::Straight));
+            if delta_x == -1 {
+                // came from east, continue west
+                neighbours.push((Coord::new(location.x - 1, location.y), Direction::Straight));
             }
         }
     }
     if delta_x == 0 {
         if can_turn {
-            neighbours.push((Coord::new(location.x+1, location.y), Direction::Turned));
-            neighbours.push((Coord::new(location.x-1, location.y), Direction::Turned));
+            neighbours.push((Coord::new(location.x + 1, location.y), Direction::Turned));
+            neighbours.push((Coord::new(location.x - 1, location.y), Direction::Turned));
         }
 
         if can_continue_straight {
-            if delta_y == 1 { // came from north, continue south
-                neighbours.push((Coord::new(location.x, location.y+1), Direction::Straight));
+            if delta_y == 1 {
+                // came from north, continue south
+                neighbours.push((Coord::new(location.x, location.y + 1), Direction::Straight));
             }
-            if delta_y == -1 { // came from south, continue north
-                neighbours.push((Coord::new(location.x, location.y-1), Direction::Straight));
+            if delta_y == -1 {
+                // came from south, continue north
+                neighbours.push((Coord::new(location.x, location.y - 1), Direction::Straight));
             }
         }
     }
@@ -88,21 +101,26 @@ fn get_potential_neighbours(location: Coord, came_from: Coord, can_continue_stra
 }
 
 fn heuristic(location: Coord, destination: Coord) -> u32 {
-    (abs(destination.x - location.x) as u32 + abs(destination.y - location.y) as u32) * 1
+    abs(destination.x - location.x) as u32 + abs(destination.y - location.y) as u32
 }
 
-fn a_star(cost_map: &Grid<u32>, start: Coord, destination: Coord, min_straight: u32, max_straight : u32) -> u32 {
-
+fn a_star(
+    cost_map: &Grid<u32>,
+    start: Coord,
+    destination: Coord,
+    min_straight: u32,
+    max_straight: u32,
+) -> u32 {
     let mut unvisited: BinaryHeap<HeapState> = BinaryHeap::new();
     unvisited.push(HeapState::new(start, start, 0, 0));
 
-    let mut f_scores : HashMap<(Coord, Coord, u32), u32> = HashMap::new();
+    let mut f_scores: HashMap<(Coord, Coord, u32), u32> = HashMap::new();
     f_scores.insert((start, start, 0), 0);
 
-    let mut g_scores : HashMap<(Coord, Coord, u32), u32> = HashMap::new();
+    let mut g_scores: HashMap<(Coord, Coord, u32), u32> = HashMap::new();
     g_scores.insert((start, start, 0), 0);
 
-    let mut came_from : HashMap<Coord, Coord> = HashMap::new();
+    let mut came_from: HashMap<Coord, Coord> = HashMap::new();
 
     loop {
         let node = unvisited.pop();
@@ -112,30 +130,57 @@ fn a_star(cost_map: &Grid<u32>, start: Coord, destination: Coord, min_straight: 
         }
         let node = node.unwrap();
 
-        let current_g_score = *g_scores.get(&(node.location, node.came_from, node.straight_line_count)).unwrap();
+        let current_g_score = *g_scores
+            .get(&(node.location, node.came_from, node.straight_line_count))
+            .unwrap();
 
         if node.location == destination {
             break current_g_score;
         }
 
-        let can_continue_straight = if node.location == start { true } else {node.straight_line_count < max_straight};
-        let can_turn = if node.location == start { true } else {node.straight_line_count >= min_straight};
+        let can_continue_straight = if node.location == start {
+            true
+        } else {
+            node.straight_line_count < max_straight
+        };
+        let can_turn = if node.location == start {
+            true
+        } else {
+            node.straight_line_count >= min_straight
+        };
 
-        for (neighbour_location, direction) in get_potential_neighbours(node.location, node.came_from, can_continue_straight, can_turn) {
+        for (neighbour_location, direction) in get_potential_neighbours(
+            node.location,
+            node.came_from,
+            can_continue_straight,
+            can_turn,
+        ) {
             if !neighbour_location.is_valid(cost_map.size()) {
                 continue;
             }
 
-            let straight_line_count = if direction == Direction::Turned { 1 } else { node.straight_line_count + 1 };
+            let straight_line_count = if direction == Direction::Turned {
+                1
+            } else {
+                node.straight_line_count + 1
+            };
             let neighbour_node = (neighbour_location, node.location, straight_line_count);
 
             let tentative_g_score = current_g_score + cost_map.get(neighbour_location).unwrap();
             if tentative_g_score < *g_scores.entry(neighbour_node).or_insert(u32::MAX) {
                 g_scores.insert(neighbour_node, tentative_g_score);
-                f_scores.insert(neighbour_node, tentative_g_score + heuristic(node.location, destination));
+                f_scores.insert(
+                    neighbour_node,
+                    tentative_g_score + heuristic(node.location, destination),
+                );
                 came_from.insert(neighbour_location, node.location);
 
-                unvisited.push(HeapState::new(neighbour_location, node.location, straight_line_count, *f_scores.get(&neighbour_node).unwrap()));
+                unvisited.push(HeapState::new(
+                    neighbour_location,
+                    node.location,
+                    straight_line_count,
+                    *f_scores.get(&neighbour_node).unwrap(),
+                ));
             }
         }
     }
@@ -157,15 +202,15 @@ fn parse_input(input: &str) -> Grid<u32> {
 #[aoc(day17, part1)]
 fn part1(grid: &Grid<u32>) -> u32 {
     let start = Coord::new(0, 0);
-    let destination = Coord::new(grid.width() as i32 - 1, grid.height() as i32 -1);
-    a_star(&grid, start, destination, 0, 3)
+    let destination = Coord::new(grid.width() as i32 - 1, grid.height() as i32 - 1);
+    a_star(grid, start, destination, 0, 3)
 }
 
 #[aoc(day17, part2)]
 fn part2(grid: &Grid<u32>) -> u32 {
     let start = Coord::new(0, 0);
-    let destination = Coord::new(grid.width() as i32 - 1, grid.height() as i32 -1);
-    a_star(&grid, start, destination, 4, 10)
+    let destination = Coord::new(grid.width() as i32 - 1, grid.height() as i32 - 1);
+    a_star(grid, start, destination, 4, 10)
 }
 
 #[cfg(test)]
@@ -192,9 +237,9 @@ mod tests {
     fn test1() {
         let grid = parse_input(INPUT);
         let start = Coord::new(0, 0);
-        let destination = Coord::new(grid.width() as i32 - 1, grid.height() as i32 -1);
+        let destination = Coord::new(grid.width() as i32 - 1, grid.height() as i32 - 1);
 
-        let heat_loss = a_star(&grid, start, destination, 1, 3);
+        let heat_loss = a_star(grid, start, destination, 1, 3);
 
         assert_eq!(heat_loss, 102);
     }
